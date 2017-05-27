ed86ba4addbc454dc56f67ad3d3d33c2fa2700bf
Arthit <arr2817@gmail.com>
2017-05-27 12:06:03 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -27,7 +27,7 @@ echo $result;
 			// Build message to reply back
 			$messages = [
 				'type' => 'text',
-				'text' => $text." xxyy ".$result
+				'text' => $text." http://172.31.33.130/test/index.php ".$result
 			];
 			// Make a POST Request to Messaging API to reply to sender
 			$url = 'https://api.line.me/v2/bot/message/reply';
