4d1d2c1f3a4f1012132f7ce4a402f31a35fd4b88
Arthit <arr2817@gmail.com>
2017-05-30 23:50:30 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -49,32 +49,6 @@ echo $result;
 			echo $result . "\r\n";
 		}
 	}
-		{
-  "type": "template",
-  "altText": "this is a buttons template",
-  "template": {
-      "type": "buttons",
-      "thumbnailImageUrl": " http://202.29.80.36/bizapp/skf_store/",
-      "title": "Menu",
-      "text": "Please select",
-      "actions": [
-          {
-            "type": "postback",
-            "label": "Buy",
-            "data": "action=buy&itemid=123"
-          },
-          {
-            "type": "postback",
-            "label": "Add to cart",
-            "data": "action=add&itemid=123"
-          },
-          {
-            "type": "uri",
-            "label": "View detail",
-            "uri": "http://example.com/page/123"
-          }
-      ]
-  }
 }
-}
+
 echo "OK";
